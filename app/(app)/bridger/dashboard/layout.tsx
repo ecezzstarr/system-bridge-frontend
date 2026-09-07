748b8c4e03802534510f6e876ba14de923716002
ecezzstarr <ecezzstarr@gmail.com>
2026-09-07 23:16:41 +0100

    Connect Bridger Daily Prospect and Company Support

--- app/(app)/bridger/dashboard/layout.tsx
+++ app/(app)/bridger/dashboard/layout.tsx
@@ -1,9 +1,13 @@
-import { DailyProjectClaim } from '@/components/bridger/daily-project-claim'
+import { DailyProspect } from '@/components/bridger/daily-prospect'
+import BridgerSupportSidebar from '@/components/bridger-support-sidebar'
+import { BridgerSupportAd } from '@/components/bridger-company-support'
 
 export default function BridgerDashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <DailyProjectClaim />
+      <DailyProspect />
+      <BridgerSupportSidebar />
+      <BridgerSupportAd />
       {children}
     </>
   )
