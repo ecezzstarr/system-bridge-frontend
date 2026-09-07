f4a83a2759cac7d9ec5d7432afc502dd3005a1e7
ecezzstarr <ecezzstarr@gmail.com>
2026-09-07 22:40:21 +0100

    Place File Folder deposit and pricing inside System Switch

--- app/system-switch/page.tsx
+++ app/system-switch/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import FileFolderPurchase from '@/components/system-switch/file-folder-purchase'
 
 const SystemSwitchWorld = dynamic(() => import('@/components/system-switch/system-switch-world'), { ssr: false })
 
@@ -8,6 +9,7 @@ export default function SystemSwitchPage() {
   return (
     <main className="min-h-screen bg-black p-3 md:p-6">
       <SystemSwitchWorld />
+      <FileFolderPurchase />
     </main>
   )
 }
