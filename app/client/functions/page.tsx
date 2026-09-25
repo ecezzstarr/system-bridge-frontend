e7cf92b2454cce2c18c4d00ec5678dbaf82785e2
ecezzstarr <207387158+ecezzstarr@users.noreply.github.com>
2026-09-25 10:22:28 +0100

    Add Client functions page

---
+++ app/client/functions/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import LegacyClientDashboard from '@/components/client/client-terminal-legacy'
+
+export default function ClientFunctionsPage() {
+  return (
+    <div className="mx-auto w-full max-w-5xl p-3 pb-24 sm:p-4 md:p-6">
+      <LegacyClientDashboard />
+    </div>
+  )
+}
